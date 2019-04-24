63f6a70940308fd7e99f8836f6aa646de0b87bc3
Stepan <n.stepan.97@mail.ru>
2019-04-24 14:40:39 +0400

    fixed server script

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/survey'));
 
 app.get('/*', function(req,res) {  
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/survey/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
